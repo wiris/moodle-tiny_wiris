38dc4d8704aa809f94d0e0b7fc3ef4d8a3781f6b
icaparros-at-wiris <isaac.caparros@wiris.com>
2023-04-14 10:10:49 +0200

    build: rebuild amd package

--- lang/en/tiny_wiris.php
+++ lang/en/tiny_wiris.php
@@ -2,4 +2,4 @@
 
 $string['pluginname'] = 'MathType by WIRIS';
 $string['error_connection'] = 'An error occurred while loading MathType';
-$string['privacy:metadata'] = 'MathType for Atto does not effect or store any data itself.';
+$string['privacy:metadata'] = 'MathType for TinyMCE 6 does not effect or store any data itself.';
