3e77bb23ba7c670fc883f6c45388e31bc5a8b5c2
Ubaldo Santos <164866893+usantos-at-wiris@users.noreply.github.com>
2024-08-19 12:58:35 +0200

    lint: execute PHP beautifier on all PHP to apply MOODLE coding standard
    Task Id: KB-47193

--- classes/privacy/provider.php
+++ classes/privacy/provider.php
@@ -17,7 +17,7 @@
 /**
  * Privacy class for MathType for TinyMCE.
  *
- * @package    filter_wiris
+ * @package    tiny_wiris
  * @copyright  WIRIS Europe (Maths for more S.L)
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
@@ -39,7 +39,7 @@ class provider implements \core_privacy\local\metadata\null_provider {
      *
      * @return  string
      */
-    public static function get_reason() : string {
+    public static function get_reason(): string {
         return 'privacy:metadata';
     }
 }
