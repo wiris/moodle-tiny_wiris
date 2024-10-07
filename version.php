c0f1a12e15bd003a893e5d3b0eccc123060cf4df
Ubaldo Santos <164866893+usantos-at-wiris@users.noreply.github.com>
2024-10-07 15:45:44 +0200

    build: publish #8.8.0

--- version.php
+++ version.php
@@ -26,9 +26,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2024082700;
-$plugin->release = '8.7.1';
+$plugin->version = 2024100700;
+$plugin->release = '8.8.0';
 $plugin->requires = 2012120300;
 $plugin->component = 'tiny_wiris';
-$plugin->dependencies = ['filter_wiris' => 2024082700];
+$plugin->dependencies = ['filter_wiris' => 2024100700];
 $plugin->maturity = MATURITY_STABLE;
